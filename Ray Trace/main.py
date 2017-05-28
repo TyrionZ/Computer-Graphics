b67b120b3d8c64986cbb5dd1020067e698737fec
DESKTOP-U0BNT36\wulal <wulala979@outlook.com>
2017-05-28 16:48:05 +0800

    Add Ray Trace.

---
+++ Ray Trace/main.py
@@ -0,0 +1,181 @@
+from PIL import Image
+from PIL import ImageDraw
+import math
+import numpy
+import copy
+
+width = 300
+background = (0, 0, 0, 255)
+maxReflect = 3
+
+im = Image.new("RGBA", (width, width), background)
+pen = ImageDraw.Draw(im)
+
+def normalize(vec):
+	return vec / math.sqrt(vec.dot(vec))
+
+lightDir = normalize(numpy.array((1, 1, 1)))
+lightColor = numpy.array((1, 1, 1))
+
+def getColor(vec):
+	rgba = list(vec * 255) + [255]
+	return tuple(map(int, rgba))
+
+class Ray:
+	def __init__(self, origin, direction):
+		self.origin = origin
+		self.direction = direction
+	
+	def getPoint(self, t):
+		return self.origin + self.direction * t
+
+class Sphere:
+	def __init__(self, center, radius):
+		self.center = center
+		self.radius = radius
+
+	def intersect(self, ray):
+		v = ray.origin - self.center
+		a0 = v.dot(v) - self.radius * self.radius
+		dotProduct = ray.direction.dot(v)
+
+		if dotProduct <= 0:
+			discr = dotProduct * dotProduct - a0
+			if discr >= 0:
+				return (self, -dotProduct - math.sqrt(discr), ray.getPoint(-dotProduct - math.sqrt(discr)), normalize(ray.getPoint(-dotProduct - math.sqrt(discr)) - self.center))
+			
+		return -1
+
+class Plane:
+	def __init__(self, normal, d):
+		self.normal = normal
+		self.d = d
+	
+	def intersect(self, ray):
+		a = ray.direction.dot(self.normal)
+		if a >= 0:
+			return -1
+
+		b = self.normal.dot(ray.origin - self.normal * self.d)
+		return (self, -b / a, ray.getPoint(-b / a), self.normal)
+
+class View:
+	def __init__(self, eye, front, up, fov): #fov field of view
+		self.eye = eye
+		self.front = front
+		self.up = up
+		self.fov = fov
+		self.right = numpy.cross(front, up)
+		self.up = numpy.cross(self.right, front)
+		self.scale = math.tan(fov  * math.pi / 180) * 2
+	
+	def getViewRay(self, x, y):
+		r = self.right * ((x - 0.5) * self.scale)
+		u = self.up * ((y - 0.5) * self.scale)
+		return Ray(self.eye, normalize(self.front + r + u))
+
+class CheckerMaterial:
+	def __init__(self, scale, reflectiveness):
+		self.scale = scale
+		self.reflectiveness = reflectiveness
+	
+	def sample(self, viewRay, position, normal):
+		return numpy.array((0, 0, 0)) if abs((math.floor(position[0] * 0.1) + math.floor(position[2] * self.scale)) % 2) < 1 else numpy.array((1, 1, 1))
+
+class PhongMaterial:
+	def __init__(self, diffuse, specualr, shiniess, reflectiveness = 0):
+		self.diffuse = diffuse
+		self.specualr = specualr
+		self.shiniess = shiniess
+		self.reflectiveness = reflectiveness
+
+	def sample(self, viewRay, position, normal):
+		NdotL = normal.dot(lightDir)
+		H = normalize(lightDir - viewRay.direction)
+		NdotH = normal.dot(H)
+		diffuseTerm = self.diffuse * max(NdotL, 0)
+		specualrTerm = self.specualr * math.pow(max(NdotH, 0), self.shiniess)
+		return lightColor * (diffuseTerm + specualrTerm)
+
+def intersect(scene, ray):
+	tmpResult = (x.intersect(ray) for x in scene)
+	result = -1
+	for x in tmpResult:
+		if x is not -1:
+			if (result is -1):
+				result = x
+			elif (result[1] > x[1]):
+				result = x
+	return result
+
+def renderDepth(view, scene, maxDepth):
+	for y in range(width):
+		sy = 1 - y / width
+		for x in range(width):
+			sx = x / width
+			ray = view.getViewRay(sx, sy)
+			result = scene.intersect(ray)
+			if result is not -1:
+				depth = int(255 - min(result[1] / maxDepth * 255, 255))
+				pen.point((x, y), (depth, depth, depth, 255))
+
+def renderNormal(view, scene):
+	for y in range(width):
+		sy = 1 - y / width
+		for x in range(width):
+			sx = x / width
+			ray = view.getViewRay(sx, sy)
+			result = scene.intersect(ray)
+			if result is not -1:
+				pen.point((x, y), getColor(result[3]))
+
+
+def rayTrace(scene, ray, maxReflect):
+	result = intersect(scene, ray)
+
+	if result is not -1:
+		reflectiveness = result[0].material.reflectiveness
+		color = result[0].material.sample(ray, result[2], result[3]) * (1 - reflectiveness)
+
+		if (reflectiveness > 0) and (maxReflect > 0):
+			r = result[3] * (-2 * result[3].dot(ray.direction)) + ray.direction
+			reflectColor = rayTrace(scene, Ray(result[2], r), maxReflect - 1)
+			color = color + reflectColor * reflectiveness
+		
+		return color
+	else:
+		return numpy.array((0, 0, 0))
+
+def render(view, scene):
+	for y in range(width):
+		sy = 1 - y / width
+		for x in range(width):
+			sx = x / width
+			ray = view.getViewRay(sx, sy)
+			tcolor = rayTrace(scene, ray, maxReflect)
+			color = []
+			for i in range(3):
+				color += [min(tcolor[i], 1)]
+			color = numpy.array(color)
+			pen.point((x, y), getColor(color))
+
+# sphere = Sphere(numpy.array((0, 0, -10)), 10)
+
+# renderDepth(view, sphere, 20)
+# renderNormal(view, sphere)
+
+view = View(numpy.array((0, 5, 15)), numpy.array((0, 0, -1)), numpy.array((0, 1, 0)), 45)
+
+plane = Plane(numpy.array((0, 1, 0)), 0)
+sphere0 = Sphere(numpy.array((-10, 10, -10)), 10)
+sphere1 = Sphere(numpy.array((10, 10, -10)), 10)
+plane.material = CheckerMaterial(0.1, 0.25)
+sphere0.material = PhongMaterial(numpy.array((1, 0, 0)), numpy.array((1, 1, 1)), 16, 0.5)
+sphere1.material = PhongMaterial(numpy.array((0, 0, 1)), numpy.array((1, 1, 1)), 16, 0.25)
+scene = (plane, sphere0, sphere1)
+
+render(view, scene)
+im.show()
+
+			
+		
---
+++ submit_3/源.cpp
@@ -0,0 +1,283 @@
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <iostream>
+
+#define GLEW_STATIC
+#include <GL/glew.h>
+#include <GL/glfw3.h>
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+#include <SOIL.h>
+
+const GLuint SCR_WIDTH = 800, SCR_HEIGHT = 600;
+
+class Shader
+{
+public:
+	GLuint Program;
+
+	Shader(const GLchar* vertexPath, const GLchar* fragmentPath)
+	{
+		std::string vertexCode;
+		std::string fragmentCode;
+		std::ifstream vShaderFile;
+		std::ifstream fShaderFile;
+
+		vShaderFile.exceptions(std::ifstream::badbit);
+		fShaderFile.exceptions(std::ifstream::badbit);
+		try
+		{
+			vShaderFile.open(vertexPath);
+			fShaderFile.open(fragmentPath);
+			std::stringstream vShaderStream, fShaderStream;
+
+			vShaderStream << vShaderFile.rdbuf();
+			fShaderStream << fShaderFile.rdbuf();
+
+			vShaderFile.close();
+			fShaderFile.close();
+
+			vertexCode = vShaderStream.str();
+			fragmentCode = fShaderStream.str();
+		}
+		catch (std::ifstream::failure e)
+		{
+			std::cout << "ERROR::SHADER::FILE_NOT_SUCCESFULLY_READ" << std::endl;
+		}
+		const GLchar* vShaderCode = vertexCode.c_str();
+		const GLchar * fShaderCode = fragmentCode.c_str();
+
+		GLuint vertex, fragment;
+		GLint success;
+		GLchar infoLog[512];
+
+		vertex = glCreateShader(GL_VERTEX_SHADER);
+		glShaderSource(vertex, 1, &vShaderCode, NULL);
+		glCompileShader(vertex);
+
+		glGetShaderiv(vertex, GL_COMPILE_STATUS, &success);
+		if (!success)
+		{
+			glGetShaderInfoLog(vertex, 512, NULL, infoLog);
+			std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+		}
+
+		fragment = glCreateShader(GL_FRAGMENT_SHADER);
+		glShaderSource(fragment, 1, &fShaderCode, NULL);
+		glCompileShader(fragment);
+
+		glGetShaderiv(fragment, GL_COMPILE_STATUS, &success);
+		if (!success)
+		{
+			glGetShaderInfoLog(fragment, 512, NULL, infoLog);
+			std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
+		}
+
+		this->Program = glCreateProgram();
+		glAttachShader(this->Program, vertex);
+		glAttachShader(this->Program, fragment);
+		glLinkProgram(this->Program);
+
+		glGetProgramiv(this->Program, GL_LINK_STATUS, &success);
+		if (!success)
+		{
+			glGetProgramInfoLog(this->Program, 512, NULL, infoLog);
+			std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" << infoLog << std::endl;
+		}
+		glDeleteShader(vertex);
+		glDeleteShader(fragment);
+
+	}
+
+	void Use()
+	{
+		glUseProgram(this->Program);
+	}
+};
+
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode)
+{
+	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, GL_TRUE);
+}
+GLuint loadTexture(GLchar* path);
+void RenderQuad();
+
+
+int main()
+{
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+
+	GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "Bump Mapping", nullptr, nullptr);
+	glfwMakeContextCurrent(window);
+
+	glfwSetKeyCallback(window, key_callback);
+
+	glewExperimental = GL_TRUE;
+	glewInit();
+
+	glViewport(0, 0, SCR_WIDTH, SCR_HEIGHT);
+
+	glEnable(GL_DEPTH_TEST);
+
+	Shader shader("VertexShader.glsl", "FragmentShader.glsl");
+
+	GLuint diffuseMap = loadTexture("brickwall.jpg");
+	GLuint normalMap = loadTexture("brickwall_normal.jpg");
+
+	shader.Use();
+	glUniform1i(glGetUniformLocation(shader.Program, "diffuseMap"), 0);
+	glUniform1i(glGetUniformLocation(shader.Program, "normalMap"), 1);
+
+	glm::vec3 lightPos(0.5f, 1.0f, 0.3f);
+	glm::vec3 viewPos(0.f, 0.f, -2.f);
+
+	while (!glfwWindowShouldClose(window))
+	{
+		glfwPollEvents();
+
+		glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+		shader.Use();
+		glm::mat4 view = glm::lookAt(viewPos, glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, 1.0f, 0.0f));
+		glm::mat4 projection = glm::perspective(45.0f, (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
+		glUniformMatrix4fv(glGetUniformLocation(shader.Program, "view"), 1, GL_FALSE, glm::value_ptr(view));
+		glUniformMatrix4fv(glGetUniformLocation(shader.Program, "projection"), 1, GL_FALSE, glm::value_ptr(projection));
+
+		glm::mat4 model;
+		glUniformMatrix4fv(glGetUniformLocation(shader.Program, "model"), 1, GL_FALSE, glm::value_ptr(model));
+		glUniform3fv(glGetUniformLocation(shader.Program, "lightPos"), 1, &lightPos[0]);
+		glUniform3fv(glGetUniformLocation(shader.Program, "viewPos"), 1, &viewPos[0]);
+		glActiveTexture(GL_TEXTURE0);
+		glBindTexture(GL_TEXTURE_2D, diffuseMap);
+		glActiveTexture(GL_TEXTURE1);
+		glBindTexture(GL_TEXTURE_2D, normalMap);
+		RenderQuad();
+
+		model = glm::mat4();
+		model = glm::translate(model, lightPos);
+		model = glm::scale(model, glm::vec3(0.1f));
+		glUniformMatrix4fv(glGetUniformLocation(shader.Program, "model"), 1, GL_FALSE, glm::value_ptr(model));
+		RenderQuad();
+
+		glfwSwapBuffers(window);
+	}
+
+	glfwTerminate();
+	return 0;
+}
+
+GLuint quadVAO = 0;
+GLuint quadVBO;
+void RenderQuad()
+{
+	if (quadVAO == 0)
+	{
+		glm::vec3 pos1(-1.0, 1.0, 0.0);
+		glm::vec3 pos2(-1.0, -1.0, 0.0);
+		glm::vec3 pos3(1.0, -1.0, 0.0);
+		glm::vec3 pos4(1.0, 1.0, 0.0);
+
+		glm::vec2 uv1(0.0, 1.0);
+		glm::vec2 uv2(0.0, 0.0);
+		glm::vec2 uv3(1.0, 0.0);
+		glm::vec2 uv4(1.0, 1.0);
+
+		glm::vec3 nm(0.0, 0.0, 1.0);
+
+		glm::vec3 tangent1, bitangent1;
+		glm::vec3 tangent2, bitangent2;
+
+		glm::vec3 edge1 = pos2 - pos1;
+		glm::vec3 edge2 = pos3 - pos1;
+		glm::vec2 deltaUV1 = uv2 - uv1;
+		glm::vec2 deltaUV2 = uv3 - uv1;
+
+		GLfloat f = 1.0f / (deltaUV1.x * deltaUV2.y - deltaUV2.x * deltaUV1.y);
+
+		tangent1.x = f * (deltaUV2.y * edge1.x - deltaUV1.y * edge2.x);
+		tangent1.y = f * (deltaUV2.y * edge1.y - deltaUV1.y * edge2.y);
+		tangent1.z = f * (deltaUV2.y * edge1.z - deltaUV1.y * edge2.z);
+		tangent1 = glm::normalize(tangent1);
+
+		bitangent1.x = f * (-deltaUV2.x * edge1.x + deltaUV1.x * edge2.x);
+		bitangent1.y = f * (-deltaUV2.x * edge1.y + deltaUV1.x * edge2.y);
+		bitangent1.z = f * (-deltaUV2.x * edge1.z + deltaUV1.x * edge2.z);
+		bitangent1 = glm::normalize(bitangent1);
+
+		edge1 = pos3 - pos1;
+		edge2 = pos4 - pos1;
+		deltaUV1 = uv3 - uv1;
+		deltaUV2 = uv4 - uv1;
+
+		f = 1.0f / (deltaUV1.x * deltaUV2.y - deltaUV2.x * deltaUV1.y);
+
+		tangent2.x = f * (deltaUV2.y * edge1.x - deltaUV1.y * edge2.x);
+		tangent2.y = f * (deltaUV2.y * edge1.y - deltaUV1.y * edge2.y);
+		tangent2.z = f * (deltaUV2.y * edge1.z - deltaUV1.y * edge2.z);
+		tangent2 = glm::normalize(tangent2);
+
+		bitangent2.x = f * (-deltaUV2.x * edge1.x + deltaUV1.x * edge2.x);
+		bitangent2.y = f * (-deltaUV2.x * edge1.y + deltaUV1.x * edge2.y);
+		bitangent2.z = f * (-deltaUV2.x * edge1.z + deltaUV1.x * edge2.z);
+		bitangent2 = glm::normalize(bitangent2);
+
+		GLfloat quadVertices[] = {
+			pos1.x, pos1.y, pos1.z, nm.x, nm.y, nm.z, uv1.x, uv1.y, tangent1.x, tangent1.y, tangent1.z, bitangent1.x, bitangent1.y, bitangent1.z,
+			pos2.x, pos2.y, pos2.z, nm.x, nm.y, nm.z, uv2.x, uv2.y, tangent1.x, tangent1.y, tangent1.z, bitangent1.x, bitangent1.y, bitangent1.z,
+			pos3.x, pos3.y, pos3.z, nm.x, nm.y, nm.z, uv3.x, uv3.y, tangent1.x, tangent1.y, tangent1.z, bitangent1.x, bitangent1.y, bitangent1.z,
+
+			pos1.x, pos1.y, pos1.z, nm.x, nm.y, nm.z, uv1.x, uv1.y, tangent2.x, tangent2.y, tangent2.z, bitangent2.x, bitangent2.y, bitangent2.z,
+			pos3.x, pos3.y, pos3.z, nm.x, nm.y, nm.z, uv3.x, uv3.y, tangent2.x, tangent2.y, tangent2.z, bitangent2.x, bitangent2.y, bitangent2.z,
+			pos4.x, pos4.y, pos4.z, nm.x, nm.y, nm.z, uv4.x, uv4.y, tangent2.x, tangent2.y, tangent2.z, bitangent2.x, bitangent2.y, bitangent2.z
+		};
+
+		glGenVertexArrays(1, &quadVAO);
+		glGenBuffers(1, &quadVBO);
+		glBindVertexArray(quadVAO);
+		glBindBuffer(GL_ARRAY_BUFFER, quadVBO);
+		glBufferData(GL_ARRAY_BUFFER, sizeof(quadVertices), &quadVertices, GL_STATIC_DRAW);
+		glEnableVertexAttribArray(0);
+		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 14 * sizeof(GLfloat), (GLvoid*)0);
+		glEnableVertexAttribArray(1);
+		glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 14 * sizeof(GLfloat), (GLvoid*)(3 * sizeof(GLfloat)));
+		glEnableVertexAttribArray(2);
+		glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 14 * sizeof(GLfloat), (GLvoid*)(6 * sizeof(GLfloat)));
+		glEnableVertexAttribArray(3);
+		glVertexAttribPointer(3, 3, GL_FLOAT, GL_FALSE, 14 * sizeof(GLfloat), (GLvoid*)(8 * sizeof(GLfloat)));
+		glEnableVertexAttribArray(4);
+		glVertexAttribPointer(4, 3, GL_FLOAT, GL_FALSE, 14 * sizeof(GLfloat), (GLvoid*)(11 * sizeof(GLfloat)));
+	}
+	glBindVertexArray(quadVAO);
+	glDrawArrays(GL_TRIANGLES, 0, 6);
+	glBindVertexArray(0);
+}
+
+GLuint loadTexture(GLchar* path)
+{
+	GLuint textureID;
+	glGenTextures(1, &textureID);
+	int width, height;
+	unsigned char* image = SOIL_load_image(path, &width, &height, 0, SOIL_LOAD_RGB);
+
+	glBindTexture(GL_TEXTURE_2D, textureID);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, image);
+	glGenerateMipmap(GL_TEXTURE_2D);
+
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	glBindTexture(GL_TEXTURE_2D, 0);
+	SOIL_free_image_data(image);
+	return textureID;
+}
